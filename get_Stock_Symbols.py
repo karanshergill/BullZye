747155052e6428b07d1cd9370b914d1e5bc4469c
Karan Shergill <email@karanshergill.com>
2024-04-16 16:49:53 +0530

    fixed error thrown while creating list of symbols

--- get_Stock_Symbols.py
+++ get_Stock_Symbols.py
@@ -1,5 +1,6 @@
 import requests
 import logging
+from sqlalchemy import text
 from sqlalchemy.orm import sessionmaker
 from database_Connection import get_engine
 
@@ -16,7 +17,7 @@ def fetch_and_filter_data(url, exchange_segment, symbol_category):
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
-        
+
         return [
             # Filter data based on the variables
             {
@@ -29,39 +30,47 @@ def fetch_and_filter_data(url, exchange_segment, symbol_category):
     except requests.HTTPError as http_err:
         logging.error(f'HTTP error occurred: {http_err}')
     except Exception as err:
-        logging.error(f'An error occured: {err}')
+        logging.error(f'An error occurred: {err}')
 
-def insert_data(engine, filtered_data, market_value="nse", category_value="equity"):
-    # SQL query to insert data
-    insert_query = """
-    INSERT INTO stock_symbols (token, symbol, name, market, category)
-    VALUES (:token, :symbol, :name, :market, :category)
-    ON CONFLICT (token) DO NOTHING;  -- Assuming 'token' is a unique field
-    """
-    
+def insert_data(session, filtered_data, exchange="nse", category="equity"):
     try:
-        with sessionmaker(bind=engine)() as session:
-            session.bulk_insert_mappings(
-                mapper=None,  # SQLAlchemy automatically infers the mapper based on the given objects
-                mappings=[
+        count = 0
+        for item in filtered_data:
+            existing_symbol = session.execute(
+                text("SELECT COUNT(*) FROM stock_symbols WHERE token = :token"),
+                {'token': item['token']}
+            ).scalar()
+            if existing_symbol == 0:
+                session.execute(
+                    text("""
+                    INSERT INTO stock_symbols (token, symbol, name, exchange, category)
+                    VALUES (:token, :symbol, :name, :exchange, :category);
+                    """),
                     {
-                        **item,
-                        'market': market_value,
-                        'category': category_value
-                    } for item in filtered_data
-                ],
-                render_nulls=True,
-            )
-            session.commit()
-            logging.info("Data inserted successfully.")
+                        'token': item['token'],
+                        'symbol': item['symbol'],
+                        'name': item['name'],
+                        'exchange': exchange,
+                        'category': category
+                    }
+                )
+                count += 1
+        session.commit()
+        logging.info(f"{count} data inserted successfully.")
+        return count
     except Exception as e:
+        session.rollback()
         logging.error(f"An error occurred during the database operation: {e}")
+        return 0
 
 def main():
     engine = get_engine()
-    filtered_data = fetch_and_filter_data(url, exchange_segment, symbol_category)
-    if filtered_data is not None:
-        insert_data(engine, filtered_data)
+    Session = sessionmaker(bind=engine)
+    with Session() as session:
+        filtered_data = fetch_and_filter_data(url, exchange_segment, symbol_category)
+        if filtered_data:
+            count = insert_data(session, filtered_data)
+            print(f"Total {count} stock symbols inserted.")
 
 if __name__ == "__main__":
     main()
